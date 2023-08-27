b77be4b240e7fe49f1f3db0fd521a7c448c526a7
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-08-27 19:44:26 -0300

    feat: implements the project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediasModule } from './medias/medias.module';
+import { PostsModule } from './posts/posts.module';
+import { PublicationsModule } from './publications/publications.module';
 
 @Module({
-  imports: [],
+  imports: [MediasModule, PostsModule, PublicationsModule],
   controllers: [AppController],
   providers: [AppService],
 })

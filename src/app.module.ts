9e9fe617f463a19445265484db57ef3fc2ef4e2e
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-08-28 08:33:34 -0300

    feat and fix: dependency tweaks and inclusion of the prism module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MediasModule } from './medias/medias.module';
 import { PostsModule } from './posts/posts.module';
 import { PublicationsModule } from './publications/publications.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MediasModule, PostsModule, PublicationsModule],
+  imports: [MediasModule, PostsModule, PublicationsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
